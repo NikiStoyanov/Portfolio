7dec7e28baaf2540d69a2ee50202432541c5530c
Nikolay Stoyanov <nikistoyanov2005@gmail.com>
2022-06-22 16:21:21 +0200

    Contact form and resume

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,18 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+$receiving_email_address = 'nikistoyanov2005@gmail.com';
+$name = @trim(stripslashes($_POST['name']));
+$from = @trim(stripslashes($_POST['email']));
+$subject = @trim(stripslashes($_POST['subject']));
+$message = @trim(stripslashes($_POST['message']));
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$headers   = '';
+$headers .= "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type: text/plain; charset=iso-8859-1" . "\r\n";
+$headers .= "From: {$name} <{$from}>" . "\r\n";
+$headers .= "Reply-To: <{$from}>" . "\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion() . "\r\n";
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+mail($receiving_email_address, $subject, $message, $headers);
+header("Location:https://nikistoyanov.github.io/Portfolio/");
 ?>
